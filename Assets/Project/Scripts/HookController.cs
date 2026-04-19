312163342a9fb2efb3a0127f8f54e34b2dfb33c9
Vyacheslav Aleksandrov <aleksandrov@owlcat.games>
2026-04-19 15:24:53 +0300

    Add Hook

---
+++ Assets/Project/Scripts/HookController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class HookController : MonoBehaviour
+{
+    public Transform hookPoint;
+    public Animator animator;
+    public LayerMask Hookable;
+
+    private Rigidbody2D carriedObject;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetBool("isHooking", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Drop();
+        }
+    }
+
+    // ���������� �� ��������!
+    public void Pick()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(hookPoint.position, 1f, Hookable);
+
+        if (hit != null && hit.attachedRigidbody != null)
+        {
+            carriedObject = hit.attachedRigidbody;
+
+            carriedObject.linearVelocity = Vector2.zero;
+            carriedObject.bodyType = RigidbodyType2D.Kinematic;
+
+            carriedObject.transform.parent = hookPoint;
+            carriedObject.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    public void Drop()
+    {
+        if (carriedObject != null)
+        {
+            carriedObject.transform.parent = null;
+            carriedObject.bodyType = RigidbodyType2D.Dynamic;
+
+            carriedObject = null;
+        }
+
+        animator.SetBool("isHooking", false);
+    }
+}
