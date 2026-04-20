9ece159836335d880b7ea2201bc762b3f3677897
Vyacheslav Aleksandrov <aleksandrov@owlcat.games>
2026-04-20 19:42:11 +0300

    ADD sound of thunder

--- Assets/Project/Scripts/Car/CarParts/LightningRod/LightningVolume.cs
+++ Assets/Project/Scripts/Car/CarParts/LightningRod/LightningVolume.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class LightningVolume : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         var car = CarActions.instance;
@@ -9,6 +17,8 @@ public class LightningVolume : MonoBehaviour
         if (other.attachedRigidbody == car.carBody)
         {
             car.lightningRodController.lightningVolume = this;
+            StartCoroutine(PlayThunder());
+
         }
     }
 
@@ -19,6 +29,20 @@ public class LightningVolume : MonoBehaviour
         if (other.attachedRigidbody == car.carBody)
         {
             car.lightningRodController.lightningVolume = null;
+            StopAllCoroutines();
+            audioSource.Stop();
         }
     }
+
+    IEnumerator PlayThunder()
+    {
+        while (true)
+        {
+            audioSource.pitch = Random.Range(0.9f, 1.2f);
+            audioSource.Play();
+
+            yield return new WaitUntil(() => !audioSource.isPlaying);
+        }
+    }
+
 }
