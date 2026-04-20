aafb667d022f0f2b9a3b9b34914ac38f16e369db
Vyacheslav Aleksandrov <aleksandrov@owlcat.games>
2026-04-20 22:18:04 +0300

    Add sound of gate

--- Assets/Project/Scripts/Gate/PlatformButton.cs
+++ Assets/Project/Scripts/Gate/PlatformButton.cs
@@ -2,7 +2,13 @@
 using UnityEngine;
 
 public class PlatformButton : MonoBehaviour
-{ 
+{
+    public AudioSource audioSource;
+    public AudioClip[] gateClips;
+
+    public float pitchMin = 0.9f;
+    public float pitchMax = 1.1f;
+
     [SerializeField] private GateController gateController;
 
     [SerializeField] private Sprite spriteDefault;
@@ -15,9 +21,14 @@ public class PlatformButton : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (isPressed == false && !audioSource.isPlaying)
+        {
+            PlayGateAudio();
+        }
         pressedCount++;
 
         UpdatePressed();
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -25,11 +36,24 @@ public class PlatformButton : MonoBehaviour
         pressedCount--;
 
         UpdatePressed();
+
+        if (isPressed == false)
+        {
+            audioSource.Stop();
+        }
     }
 
     private void UpdatePressed()
     {
         gateController.isOpening = isPressed;
         spriteRenderer.sprite = isPressed ? spritePressed : spriteDefault;
+    }
+
+    void PlayGateAudio()
+    {
+        AudioClip clip = gateClips[UnityEngine.Random.Range(0, gateClips.Length)];
+        audioSource.pitch = UnityEngine.Random.Range(pitchMin, pitchMax);
+        audioSource.PlayOneShot(clip);
+
     }
 }
