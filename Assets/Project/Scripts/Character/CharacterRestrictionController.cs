b0a5cbad07783b583da17c9480986659233c0c78
chimpoka <chimpoka@list.ru>
2026-04-20 22:16:50 +0500

    Add car restrictions logic

---
+++ Assets/Project/Scripts/Character/CharacterRestrictionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CharacterRestrictionController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        var restrictions = CarActions.instance.carRestrictions;
+
+        if (transform.position.x > restrictions.rightRestriction.position.x ||
+            transform.position.x < restrictions.leftRestriction.position.x ||
+            transform.position.y > restrictions.upRestriction.position.y ||
+            transform.position.y < restrictions.downRestriction.position.y)
+        {
+            transform.position = restrictions.characterRespawnPoint.position;
+        }
+    }
+}
