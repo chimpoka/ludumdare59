0099c6f67fdc3b1c8d6bfcc3f3f45a29929faf11
chimpoka <chimpoka@list.ru>
2026-04-20 08:11:12 +0500

    Add gate and platform logic

---
+++ Assets/Project/Scripts/GateController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    [SerializeField] private Transform movingTransform;
+    [SerializeField] private float openSpeed;
+    [SerializeField] private float closeSpeed;
+    [SerializeField] private float maxOpenPosition;
+    [SerializeField] private float minClosePosition;
+    
+    [NonSerialized] public bool isOpening;
+
+    private void FixedUpdate()
+    {
+        var pos = movingTransform.position;
+        
+        if (isOpening)
+        {
+            pos.y += openSpeed;
+            pos.y = Mathf.Max(pos.y, maxOpenPosition);
+        }
+        else
+        {
+            pos.y -= closeSpeed;
+            pos.y = Mathf.Min(pos.y, minClosePosition);
+        }
+        
+        movingTransform.position = pos;
+    }
+}
---
+++ Assets/Project/Scripts/PlatformButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class PlatformButton : MonoBehaviour
+{ 
+    [SerializeField] private GateController gateController;
+
+    [SerializeField] private Sprite spriteDefault;
+    [SerializeField] private Sprite spritePressed;
+    
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    [NonSerialized] public bool isPressed;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        spriteRenderer.sprite = spritePressed;
+        
+        isPressed = true;
+        gateController.isOpening = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        spriteRenderer.sprite = spriteDefault;
+        
+        isPressed = false;
+        gateController.isOpening = false;
+    }
+}
